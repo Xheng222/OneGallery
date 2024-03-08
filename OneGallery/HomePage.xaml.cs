ab318b1f314c76a62d621aeb7e85b4ca7e3eb0ba
Xheng222 <xheng222@163.com>
2024-03-08 20:13:26 +0800

    Day5

--- OneGallery/HomePage.xaml.cs
+++ OneGallery/HomePage.xaml.cs
@@ -9,9 +9,12 @@ using Microsoft.UI.Xaml.Controls.Primitives;
 using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Media.Animation;
 using Microsoft.UI.Xaml.Navigation;
+using Windows.ApplicationModel.Contacts;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Foundation.Metadata;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -26,6 +29,26 @@ namespace OneGallery
         public HomePage()
         {
             this.InitializeComponent();
+        }
+        public void PrepareConnectedAnimation(ConnectedAnimationConfiguration config)
+        {
+            var anim = ConnectedAnimationService.GetForCurrentView().PrepareToAnimate("ForwardConnectedAnimation", SourceElement);
+
+            if (config != null && ApiInformation.IsApiContractPresent("Windows.Foundation.UniversalApiContract", 7))
+            {
+                anim.Configuration = config;
+            }
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            var anim = ConnectedAnimationService.GetForCurrentView().GetAnimation("BackwardConnectedAnimation");
+            if (anim != null)
+            {
+                anim.TryStart(SourceElement);
+            }
         }
     }
 }
--- OneGallery/MainWindow.xaml.cs
+++ OneGallery/MainWindow.xaml.cs
@@ -1,10 +1,14 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -15,35 +19,52 @@ using Microsoft.UI.Xaml.Media.Animation;
 using Microsoft.UI.Xaml.Navigation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.ApplicationSettings;
+using WinRT;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace OneGallery
 {
+    public class Category
+    {
+        public string Name { get; set; }
+
+        public FontIcon Icon = new();
+
+        public ObservableCollection<Category> Children { get; set; }
+        public bool IsLeaf { get; set; }
+
+        public Category() 
+        {
+            Icon.Glyph = "\uE80F";
+        }
+    }
 
     public sealed partial class MainWindow : Window
     {
-        TreeViewNode personalFolder;
+        readonly string appTitleText = "OneGallery";
 
-        readonly String appTitleText = "OneGallery";
-        Stack view_history = new();
+        private Stack<string> HistoryPage = new();
+
+        Stack<string> PartenPagemName = new();
+
+        Dictionary<string, string> ParentDictionary = new Dictionary<string, string>();
+
+        Dictionary<string, NavigationViewItem> PageDictionary = new Dictionary<string, NavigationViewItem>();
+
+        Dictionary<string, Category> NvItemDictionary = new Dictionary<string, Category>();
 
         public MainWindow()
         {
             this.InitializeComponent();
-
-
 
             this.ExtendsContentIntoTitleBar = true;
 
             this.TrySetAcrylicBackdrop();
 
             this.SetTitleBar(AppTitleBar);
-
-            Nv.SelectedItem = Nv.MenuItems[0];
-            NavView_Navigate(typeof(HomePage), new EntranceNavigationTransitionInfo());
-            // this.InitializeSampleTreeView();
 
 
         }
@@ -60,89 +81,287 @@ namespace OneGallery
             return false; // Acrylic is not supported on this system
         }
 
-        private void NavView_Navigate(
-            Type navPageType,
-            NavigationTransitionInfo transitionInfo)
+        private void NavView_Navigate(Type navPageType)
         {
             // Get the page type before navigation so you can prevent duplicate
             // entries in the backstack.
             Type preNavPageType = Nv_page.CurrentSourcePageType;
 
             // Only navigate if the selected page isn't currently loaded.
-            if (navPageType is not null && !Type.Equals(preNavPageType, navPageType))
+            if (navPageType is not null && !Equals(preNavPageType, navPageType))
             {
-                Nv_page.Navigate(navPageType, null, transitionInfo);
+                Nv_page.Navigate(navPageType, null);
+
             }
         }
 
-        private bool Nv_ChangePage(String page)
+        private void NavView_ItemInvoked(NavigationView sender,
+                                 NavigationViewItemInvokedEventArgs args)
         {
-            Debug.Print(page + "\n");
-            if (page == "HomePage" || page == "SettingPage" || page == "SamplePage2")
+            Type CurrentPage = Nv_page.CurrentSourcePageType;
+            if (CurrentPage != null)
             {
-                string pageName = "OneGallery." + page;
-                Type pageType = Type.GetType(pageName);
-                Nv_page.Navigate(pageType);
-                view_history.Push(page);
-                return true;
+
+                if (args.IsSettingsInvoked == true)
+                {
+                    if (!string.Equals(CurrentPage.Name, "Settings"))
+                    {
+                        Debug.Print("history " + CurrentPage.Name);
+                        HistoryPage.Push(CurrentPage.Name);
+                    }
+                }
+                else if (args.InvokedItemContainer != null)
+                {
+                    string ClickedPageName = args.InvokedItemContainer.Tag.ToString();
+
+
+                    if (!string.Equals(CurrentPage.Name, ClickedPageName))
+                    {
+                            Debug.Print("history " + CurrentPage.Name);
+                            HistoryPage.Push(CurrentPage.Name);
+
+                    }   
+                
+                }
+
             }
-            
-            return false;
+
         }
 
-        private void Nv_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        private void NavView_SelectionChanged(NavigationView sender,
+                                      NavigationViewSelectionChangedEventArgs args)
         {
-            /* NOTE: for this function to work, every NavigationView must follow the same naming convention: nvSample# (i.e. nvSample3),
-            and every corresponding content frame must follow the same naming convention: contentFrame# (i.e. contentFrame3) */
+            string Page = args.SelectedItemContainer.Tag.ToString();
+            //Debug.Print("Select " + Page);
 
-            // Get the sample number
-            string ChoosePage = sender.Name;
-            Debug.Print(ChoosePage + "\n");
-
-            if (args.IsSettingsSelected)
+            if (args.IsSettingsSelected == true)
             {
-                Nv_ChangePage("SettingPage");
-                //Nv_page.Navigate(typeof(SettingPage));
+                NavView_Navigate(typeof(Settings));
             }
-            else
+            else if (args.SelectedItemContainer != null)
             {
-                var selectedItem = (NavigationViewItem)args.SelectedItem;
-                string page = ((string)selectedItem.Tag);
-                Nv_ChangePage(page);
-
-                //    var selectedItem = (Microsoft.UI.Xaml.Controls.NavigationViewItem)args.SelectedItem;
-                //    string selectedItemTag = ((string)selectedItem.Tag);
-                //    sender.Header = "Sample Page " + selectedItemTag.Substring(selectedItemTag.Length - 1);
-                //    string pageName = "WinUIGallery.SamplePages." + selectedItemTag;
-                //    Type pageType = Type.GetType(pageName);
-                //    contentFrame8.Navigate(pageType);
+                Category s = (Category)Nv.SelectedItem; 
+                if (s != null)
+                {
+                    Debug.Print("Select " + s.Name);
+                }
+                string pageName = "OneGallery." + Page;
+                    Type navPageType = Type.GetType(pageName);
+                    NavView_Navigate(navPageType);
+                
             }
         }
 
         private void Nv_BackRequested(NavigationView sender, NavigationViewBackRequestedEventArgs args)
         {
-            //if (view_history.Count <= 1)
+            Nv.IsBackEnabled = false;
+            var History = HistoryPage.Pop();
+
+            //if (!PageDictionary.ContainsKey(History))
             //{
-            //    return;
+            //    var rootGrid = VisualTreeHelper.GetChild(Nv, 0);
+            //    FindNaView(rootGrid);
             //}
 
-            //string last_page = (string)view_history.Pop();
-
-            //Nv_ChangePage(last_page);
-            //Frame rootFrame = Current.Content as Frame;
-
-            if (Nv_page.CanGoBack)
+            var rootGrid = VisualTreeHelper.GetChild(Nv, 0);
+            FindNaView(rootGrid);
+            foreach (var item in HistoryPage)
             {
-                Nv_page.GoBack();
-
+                Debug.Print("BackSt " + item);
             }
 
+            if (ParentDictionary[History] is not null)
+            {
+                ExpandParentPage(ParentDictionary[History]);
+            }
+
+            if (History.Equals("Settings"))
+            {
+                Nv.SelectedItem = PageDictionary[History];
+            }
+            else
+            {
+                Nv.SelectedItem = NvItemDictionary[History];
+            }
+
+            //if (ParentDictionary[History] is not null)
+            //{
+            //    CollapseParentPage(ParentDictionary[History]);
+            //}
+
+            //Nv.UpdateLayout();
+        }
+
+        public ObservableCollection<Category> Categories = new()
+        {
+            new Category() {
+            Name = "HomePage",
+                Children = new ObservableCollection<Category>() {
+                    new Category(){
+                        Name = "Menu item 2",
+                        //CategoryIcon = "Home",
+                        Children = new ObservableCollection<Category>() {
+                            new Category() {
+                                Name  = "Menu item 3",
+                                //CategoryIcon = "Home",
+                                Children = new ObservableCollection<Category>() {
+                                    new Category()
+                                    {
+                                        Name  = "Menu item 4", 
+                                        //CategoryIcon = "Home", 
+                                        IsLeaf = true
+                                    },
+                                    new Category()
+                                    {
+                                        Name = "SamplePage2"
+                                    }
+    }
+                            }
+                        }
+                    }
+                }
+            },
+            new Category(){
+                Name = "Menu item 6",
+                //CategoryIcon = "Home",
+                Children = new ObservableCollection<Category>() {
+                    new Category(){
+                        Name = "Menu item 7",
+                        //CategoryIcon = "Home",
+                        Children = new ObservableCollection<Category>() {
+                            new Category() { 
+                                Name  = "Menu item 8", 
+                                //CategoryIcon = "Home", 
+                                IsLeaf = true 
+                            },
+                            new Category() { 
+                                Name  = "Menu item 9", 
+                                //CategoryIcon = "Home", 
+                                IsLeaf = true 
+                            }
+                        }
+                    }
+                }
+            },
+            new Category(){ 
+                Name = "Menu item 10", 
+                //CategoryIcon = "Home", 
+                IsLeaf = true }
+        };
+
+        private void Nv_Page_Navigated(object sender, NavigationEventArgs e)
+        {
+            if (HistoryPage.Count > 1)
+            {
+                Nv.IsBackEnabled = true;
+            }
 
         }
 
+        private void FindNaView(DependencyObject Item)
+        {
+            var TypeName = Item.GetType().Name;
+            var ChildNum = VisualTreeHelper.GetChildrenCount(Item);
+            if (ChildNum > 0)
+            {
+                if (TypeName.Equals("NavigationViewItem"))
+                {
+                    var NaView = (NavigationViewItem)Item;
+                    var PageName = NaView.Tag.ToString();
+                    
+                    if (!PageDictionary.ContainsKey(PageName))
+                    {
+                        PageDictionary.Add(PageName, NaView);
+                    }
+                    //else
+                    //{
+                    //    PageDictionary.Remove(PageName);
+                    //    PageDictionary.Add(PageName, NaView);
+                    //}
 
+                    if (!ParentDictionary.ContainsKey(PageName))
+                    {
+                        if (PartenPagemName.Count != 0)
+                        {
+                            ParentDictionary.Add(PageName, PartenPagemName.Peek());
+                        }
+                        else
+                        {
+                            ParentDictionary.Add(PageName, null);
+                        }
+                        
+                    }
 
+                    PartenPagemName.Push(PageName);
 
+                    for (var i = 0; i < ChildNum; i++)
+                    {
+                        FindNaView(VisualTreeHelper.GetChild(Item, i));
+                    }
 
+                    PartenPagemName.Pop();
+                }
+
+                else
+                {
+                    for (var i = 0; i < ChildNum; i++)
+                    {
+                        FindNaView(VisualTreeHelper.GetChild(Item, i));
+                    }
+                }
+
+            }
+
+            return;
+        }
+
+        private void ExpandParentPage(string PageName)
+        {
+            if (ParentDictionary[PageName] != null)
+            {
+                ExpandParentPage(ParentDictionary[PageName]);
+            }
+
+            Nv.Expand(PageDictionary[PageName]);
+
+            return;
+        }
+
+        private void CollapseParentPage(string PageName)
+        {
+            Nv.Collapse(PageDictionary[PageName]);            
+            if (ParentDictionary[PageName] != null)
+            {
+                CollapseParentPage(ParentDictionary[PageName]);
+            }
+            return;
+        }
+
+        private void Nv_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            var rootGrid = VisualTreeHelper.GetChild(Grid, 0);
+            FindNaView(rootGrid);
+
+            UpdateNvItemDir(Categories);
+
+            Nv.SelectedItem = NvItemDictionary["HomePage"];
+            HistoryPage.Push("HomePage");
+        }
+
+        private void UpdateNvItemDir(ObservableCollection<Category> Items)
+        {
+            if (Items != null)
+            {
+                foreach (var Item in Items)
+                {
+                    //Debug.Print(Item.Name);
+                    NvItemDictionary.Add(Item.Name, Item);
+                    UpdateNvItemDir(Item.Children);
+                }
+
+            }
+            return;
+        }
     }
 }
